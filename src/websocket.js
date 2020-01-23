1c96e44592a643c083eb76b1aa91c6e5680f170a
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-01-22 23:50:15 -0300

    salvando as connections do socket

--- src/websocket.js
+++ src/websocket.js
@@ -1,9 +1,21 @@
 const socketio = require("socket.io");
+const parseStringToArray = require("./utils/parseStringToArray");
+
+const connections = [];
 
 exports.setupWebsocket = server => {
   const io = socketio(server);
 
   io.on("connection", socket => {
-    console.log(socket.id);
+    const { latitude, longitude } = socket.handshake.query;
+
+    connections.push({
+      id: socket.id,
+      coordinates: {
+        latitude: Number(latitude),
+        longitude: Number(longitude)
+      },
+      techs: parseStringToArray(techs)
+    });
   });
 };
