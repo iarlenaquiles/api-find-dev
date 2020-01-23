9cf0fe39af2e251054adb6f066467bba385ab6c8
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-01-23 00:18:56 -0300

    consigurando socket io

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require('cors');
+const cors = require("cors");
+const http = require("http");
 const routes = require("./routes");
+const { setupWebsocket } = require("./websocket");
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect(
   "mongodb+srv://iarlen:<pass>@cluster0-fqlyd.mongodb.net/api-find-dev?retryWrites=true&w=majority",
@@ -17,4 +22,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
